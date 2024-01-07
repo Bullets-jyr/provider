89e92ce7e7ab1f07af675bed89eb90cc47f45b04
Bullets-jyr <jyr@bullets.co.kr>
2024-01-07 19:58:04 +0900

    20. Provider - riverpod generator 1

--- lib/pages/basic/basic_provider.dart
+++ lib/pages/basic/basic_provider.dart
@@ -1,15 +1,37 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final helloProvider = Provider<String>((ref) {
+// dart run build_runner build --delete-conflicting-output
+// dart run build_runner build -d
+// dart run build_runner watch -d
+part 'basic_provider.g.dart';
+
+// final helloProvider = Provider<String>((ref) {
+//   ref.onDispose(() {
+//     print('[helloProvider] disposed');
+//   });
+//   return 'Hello';
+// });
+
+// final worldProvider = Provider<String>((ref) {
+//   ref.onDispose(() {
+//     print('[worldProvider] disposed');
+//   });
+//   return 'World';
+// });
+
+@Riverpod(keepAlive: true)
+String hello(HelloRef ref) {
   ref.onDispose(() {
     print('[helloProvider] disposed');
   });
   return 'Hello';
-});
+}
 
-final worldProvider = Provider<String>((ref) {
+@Riverpod(keepAlive: true)
+String world(WorldRef ref) {
   ref.onDispose(() {
     print('[worldProvider] disposed');
   });
   return 'World';
-});
+}
